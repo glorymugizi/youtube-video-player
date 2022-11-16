ab9ee0da3966602ad0638dcc40c72013431ffdd3
glorymugizi <glorymugizi@gmail.com>
2022-11-16 15:11:04 +0300

    created a script.js file and embede a video on the index.html file

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// const menuDisplay = document.querySelector('.menu');
+// const rightSection = document.querySelector('.right');
+// const overlay = document.querySelector('.overlay');
+
+// menuDisplay.addEventListener('click', function () {
+//   rightSection.className = 'hidden';
+// });
+// // document.querySelector('.menu').textContent = 'navbar';
