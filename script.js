fcdb50e7355b1f6df04814db37b22290cff27012
glorymugizi <glorymugizi@gmail.com>
2022-11-17 16:07:50 +0300

    many the page a little bit responsive

--- script.js
+++ script.js
@@ -8,3 +8,26 @@
 //   rightSection.className = 'hidden';
 // });
 // // document.querySelector('.menu').textContent = 'navbar';
+
+function run() {
+  var foo = 'Foo';
+  let bar = 'Bar';
+  console.log(foo, bar);
+  {
+    var moo = 'Mooo';
+    let baz = 'Bazz';
+    console.log(moo, baz);
+  }
+  {
+    console.log(moo); // Mooo
+    console.log(baz); // ReferenceError
+  }
+}
+
+const mySting = 'mugizi glory';
+console.log(mySting);
+console.log(mySting.length);
+console.log(mySting.slice(6, 10));
+console.log(mySting.toUpperCase());
+console.log(mySting.indexOf('g'));
+console.log(mySting.replace('mugizi', 'muruhura'));
